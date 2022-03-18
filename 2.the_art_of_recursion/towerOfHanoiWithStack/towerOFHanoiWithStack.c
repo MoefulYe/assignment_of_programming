a1aa0b51e5462cdeb42c578e6ee9d77d4f8ee680
ashenye <luren145@gmail.com>
2022-03-18 13:27:34 +0800

    Hanoiwithstack

--- 2.the_art_of_recursion/towerOfHanoiWithStack/towerOFHanoiWithStack.c
+++ 2.the_art_of_recursion/towerOfHanoiWithStack/towerOFHanoiWithStack.c
@@ -1,6 +1,16 @@
 #include "./stack/_stack.h"
 #define MAX_CAP 50
-int main(){
+void Hanoi(problem pb){
     _stack stack=create(MAX_CAP);
+    push(&stack,pb);
+    while(status(&stack)){
+        problem temp=pop(&stack);
+        if(temp.n==1);
+        else{
+            
+        }
+    }
+}
+int main(){
     
 }
